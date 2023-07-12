59ea6744d77a27ae15a671ef229a400448fdcd8b
Gabriel Mota <88755473+gabmotamelo@users.noreply.github.com>
2023-07-12 11:53:47 -0300

    Desafio 1 - Ordene Valores

---
+++ src/Desafio1.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Desafio1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        List<Integer> pares = new ArrayList<>();
+        List<Integer> impares = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int valor = scanner.nextInt();
+            if (valor % 2 == 0) {
+                pares.add(valor);
+            } else {
+                impares.add(valor);
+            }
+        }
+
+        Collections.sort(pares);
+        Collections.sort(impares, Collections.reverseOrder());
+
+        for (int valor : pares) {
+            System.out.println(valor);
+        }
+
+        for (int valor : impares) {
+            System.out.println(valor);
+        }
+    }
+}
